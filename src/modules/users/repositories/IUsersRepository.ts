6d3af753b86b8b3f4419a0666e6daa334e771570
RaphaelMW <raphael@mundowap.com.br>
2023-02-13 16:21:23 -0300

    fix avatar_url

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,4 +1,4 @@
-import { User } from "../infra/typeorm/entities/User";
+import { User } from "../infra/entities/User";
 import { ICreateUser } from "../interfaces/ICreateUser";
 
 export interface IUsersRepository {
